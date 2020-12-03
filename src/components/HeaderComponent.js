4943d3d1e77bade3c6dd5cff2e59ff062b60e1a6
Ashish <ashishramachandran804@gmail.com>
2020-12-02 23:19:02 -0800

    Intro to Redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -49,29 +49,29 @@ class Header extends Component {
                             <Nav navbar>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/home">
-                                        <span className="fa fa-home fa-lg"></span>Home
+                                        <span className="fa fa-home fa-lg"></span>   Home
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/aboutus">
-                                        <span className="fa fa-info fa-lg"></span>About Us
+                                        <span className="fa fa-info fa-lg"></span>   About Us
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/menu">
-                                        <span className="fa fa-list fa-lg"></span>Menu
+                                        <span className="fa fa-list fa-lg"></span>   Menu
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/contactus">
-                                        <span className="fa fa-address-card fa-lg"></span>Contact Us
+                                        <span className="fa fa-address-card fa-lg"></span>   Contact Us
                                     </NavLink>
                                 </NavItem>
                             </Nav>
                             <Nav className="ml-auto navbar">
                                 <NavItem>
                                     <Button outline onClick={this.toggleModal}>
-                                        <span className="fa fa-sign-in fa-lg"></span>Login
+                                        <span className="fa fa-sign-in fa-lg"></span>   Login
                                     </Button>
                                 </NavItem>
                             </Nav>
