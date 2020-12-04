fa888eeeb8967946a2bc5c615e9c6e763eb9d367
Ashish <ashishramachandran804@gmail.com>
2020-12-03 17:02:45 -0800

    Redux Actions

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-import CommentForm from './CommentForm';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -22,7 +21,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
         )
     }
 
-    class CommentForm2 extends Component {
+    class CommentForm extends Component {
         constructor(props) {
             super(props);
 
@@ -42,9 +41,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
 
         handleSubmit(values) {
             this.toggleModal();
-            //this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
-            console.log("Current State is: " + JSON.stringify(values));
-            alert("Current State is: " + JSON.stringify(values));
+            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
         }
 
         render() {
@@ -117,7 +114,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
         }
     }
 
-    function RenderComments({comments}) {
+    function RenderComments({comments, addComment, dishId}) {
         if(comments != null) {
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -132,7 +129,7 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
                             )
                         })}
                     </ul>
-                    <CommentForm2/>
+                    <CommentForm dishId={dishId} addComment={addComment}/>
                 </div>
             );
         } else {
@@ -159,7 +156,10 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
                         </div>
                         <div className="row">
                             <RenderDish dish={props.dish} />
-                            <RenderComments comments={props.comments} />
+                            <RenderComments comments={props.comments}
+                                addComment={props.addComment}
+                                dishId={props.dish.id}
+                            />
                         </div>
                 </div>
             );
